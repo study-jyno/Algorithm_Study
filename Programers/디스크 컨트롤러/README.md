a4f62c890eafb242d3efeb7fe495c32cf98415d0
jyno <jinho9613@gmail.com>
2021-11-17 17:37:09 +0900

    전체 폴더 구조 변경

---
+++ Programers/디스크 컨트롤러/README.md
@@ -0,0 +1,5 @@
+# Link
+https://programmers.co.kr/learn/courses/30/lessons/42627?language=python3
+
+# 구현 방법
+힙을 사용해 정렬 수 해당 시간에 실행할 수 있는 작업 중 처리 시간이 가장 짧은 일부터 처리
---
+++ Programers/디스크 컨트롤러/disk_controller.py
@@ -0,0 +1,11 @@
+def solution(jobs):
+    answer = 0
+
+    return answer
+
+
+if __name__ == "__main__":
+    jobs = [[0, 3], [1, 9], [2, 6]]
+    return_value = 9
+    answer = solution(jobs)
+    print(f'answer : {answer} | result {return_value}')
