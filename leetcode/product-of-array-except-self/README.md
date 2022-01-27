dfe2a8ebd0aeae806c70088d5daec4578bc30efe
jyno <jinho9613@gmail.com>
2022-01-27 23:27:43 +0900

    product of array except self 진행 중

---
+++ leetcode/product-of-array-except-self/README.md
@@ -0,0 +1,18 @@
+# Link
+https://leetcode.com/problems/product-of-array-except-self/submissions/
+
+작성 중
+# 구현 방법
+
+```python
+
+
+```
+
+# 맞는데 왜 틀리지?
+
+
+# 다른 방법
+
+
+# 다른 방법에서 궁금한점
---
+++ leetcode/product-of-array-except-self/solution.py
@@ -0,0 +1,11 @@
+def solution():
+    answer = 0
+
+    return answer
+
+
+if __name__ == "__main__":
+    result_ = []
+
+    answer_ = solution()
+    print(f'answer : {answer_} | result {result_}')
