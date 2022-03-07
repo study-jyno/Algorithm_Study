e84119e32b6b838d8b059a2dce9b3bdd6c3810ea
jyno <jinho9613@gmail.com>
2022-03-07 16:41:09 +0900

    순위 시작

---
+++ Programers/순위/README.md
@@ -0,0 +1,21 @@
+# Link
+https://programmers.co.kr/learn/courses/30/lessons/49191?language=python3
+
+# 구현 방법
+그래프를 사용하라는데 어떻게 사용해야하는거지?
+
+
+
+
+```python
+
+
+```
+
+# 맞는데 왜 틀리지?
+
+
+# 다른 방법
+
+
+# 다른 방법에서 궁금한점
---
+++ Programers/순위/solution.py
@@ -0,0 +1,12 @@
+def solution(n, results):
+    answer = 0
+    return answer
+
+
+if __name__ == "__main__":
+    n = 5
+    results = [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]
+    result = 2
+
+    answer_ = solution(n, results)
+    print(f'answer : {answer_} | result {result}')
