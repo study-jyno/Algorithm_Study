98aa3420080a04bd247405ed70508bef8d6337ea
jyno <jinho9613@gmail.com>
2022-03-08 14:27:33 +0900

    Dijkstra 다시 풀기

--- leetcode/Network Delay Time/solution_2.py
+++ leetcode/Network Delay Time/solution_2.py
@@ -2,36 +2,41 @@ import heapq
 from typing import List
 
 
+# 다시 풀어보기
+
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # K : Start N : Start
         graph = {}
         for time in times:
             if time[0] not in graph:
                 graph[time[0]] = {}
             graph[time[0]][time[1]] = time[2]
 
-        nodes = {node: float('inf') for node in range(1, n + 1)}
-        nodes[k] = 0
+        nodes = {x: float('inf') for x in range(1, n + 1)}
+        nodes[k] = 0  # Start Node is k
+
         queue = []
-        heapq.heappush(queue, [nodes[k], k])  # 시작 노드부터 탐색 시작 하기 위함.
+        heapq.heapify(queue)
+        heapq.heappush(queue, [k, nodes[k]])  # [destination, distance]
 
-        while queue:  # queue에 남아 있는 노드가 없으면 끝
-            current_distance, current_destination = heapq.heappop(queue)  # 탐색 할 노드, 거리를 가져옴.
+        while queue:
+            current_destination, current_distance = heapq.heappop(queue)
 
-            if nodes[current_destination] < current_distance:  # 기존에 있는 거리보다 길다면, 볼 필요도 없음
+            if nodes[current_destination] < current_distance:  # 이미 최적 경로를 발견한 경우 진행할 필요 없음
                 continue
+
             if current_destination in graph:
                 for new_destination, new_distance in graph[current_destination].items():
-                    distance = current_distance + new_distance  # 해당 노드를 거쳐 갈 때 거리
-                    if distance < nodes[new_destination]:  # 알고 있는 거리 보다 작으면 갱신
+                    distance = current_distance + new_distance
+                    if distance < nodes[new_destination]:  # 기존에 알고있던 거리보다 짧은 경우에 수정 작업 진행
                         nodes[new_destination] = distance
-                        heapq.heappush(queue, [distance, new_destination])  # 다음 인접 거리를 계산 하기 위해 큐에 삽입
+                        heapq.heappush(queue, [new_destination, distance])
+
         max_distance = 0
-        for key in nodes:
-            if nodes[key] == float('inf'):
+        for item in nodes:
+            if nodes[item] == float('inf'):
                 return -1
-            max_distance = max(max_distance, nodes[key])
+            max_distance = max(max_distance, nodes[item])
 
         return max_distance
 
